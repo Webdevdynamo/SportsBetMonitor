7c8d48397082187c8506b0250729f684e20172f0
Dominic Porreca <31548004+Webdevdynamo@users.noreply.github.com>
2026-01-25 16:33:05 -0700

    adasdfasdf

--- index.php
+++ index.php
@@ -81,7 +81,7 @@ $slips = file_exists($slips_file) ? json_decode(file_get_contents($slips_file), 
         .slip-final-loss::after { border-color: transparent var(--loss-red) transparent transparent; }
 
         .status-ribbon {
-            position: absolute; top: 10px; right: -10px; transform: rotate(45deg); width: 70px;
+            position: absolute; top: 15px; right: -15px; transform: rotate(45deg); width: 70px;
             text-align: center; font-size: 0.65em; font-weight: bold; color: #fff; z-index: 11;
             text-transform: uppercase; letter-spacing: 1px; pointer-events: none;
         }
