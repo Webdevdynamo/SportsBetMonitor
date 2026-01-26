11adfee4d704f0881371bea3da1987beade6f311
Dominic Porreca <31548004+Webdevdynamo@users.noreply.github.com>
2026-01-25 20:09:21 -0700

    adfasdfaf

--- index.php
+++ index.php
@@ -237,11 +237,11 @@ $slips = file_exists($slips_file) ? json_decode(file_get_contents($slips_file), 
 
         // --- NEW: SORTING LOGIC ---
         // We create a temporary array that includes a 'isFinal' flag for sorting
+        console.log(liveData);
         mySlips.map(slip => {
-        slip.legs.every(leg => {
-            // const stats = liveData[leg.player_name] || {};
-            console.log(liveData);
-         });
+            slip.legs.every(leg => {
+                // const stats = liveData[leg.player_name] || {};
+            });
         });
         const sortedSlips = mySlips.map(slip => {
             const isFinal = slip.legs.every(leg => {
