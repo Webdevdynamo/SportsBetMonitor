29838aa8dced257a200d38736201d1501c99ac0d
Dominic Porreca <31548004+Webdevdynamo@users.noreply.github.com>
2026-01-25 17:47:08 -0700

    adfasdf

--- cron_fetch.php
+++ cron_fetch.php
@@ -106,7 +106,7 @@ foreach ($gamesToFetch as $game) {
             // --- PLAYER STATISTICS ---
             foreach ($statEntry['teamPlayerStatistics'] as $team) {
                 // print_r($teamMap);
-                print_r($team['teamId']);
+                print_r($team);
                 // Get the common name from the map we built in Stage 1
                  $currentTeamName = $teamMap[$team['teamId']] ?? "Unknown";
                 foreach ($team['playerStatistics'] as $p) {
